7954c24a838a7f7cf7063df9c063bc4a51fcbd15
Larry <lukaoma@utexas.edu>
2020-01-14 17:17:51 -0600

    add

---
+++ Assets/cameraMovement.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject GOFollow = null;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = new Vector3(GOFollow.transform.position.x,GOFollow.transform.position.y,-1);
+        transform.position = pos;
+    }
+}
---
+++ Assets/paddel.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class paddel : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float rotationSpeed = 1;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Rigidbody2D rb = GetComponent<Rigidbody2D>();
+            rb.AddForce(Vector2.up);
+            
+            transform.Rotate(Vector3.forward,rotationSpeed*Time.deltaTime);
+        }
+    }
+}
