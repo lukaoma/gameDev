857089d5b53c06dd78761521ffe33cfcc15ce08c
ilm353 <ian_mcdonald@rocketmail.com>
2020-01-14 18:01:17 -0600

    art stuff

---
+++ Assets/Player.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Player : MonoBehaviour
+{
+    private static Player _instance;
+    public static Player Instance => (_instance) ? _instance : _instance = FindObjectOfType<Player>();
+
+    [SerializeField] private float _horizontalSpeed = 5;
+    
+    private Rigidbody2D _rb;
+    private void Awake()
+    {
+        if (_instance) Destroy(gameObject);
+        _instance = this;
+
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        float horizontal = Math.Abs(Input.GetAxis("Horizontal"));
+        if (Mathf.Abs(horizontal) > float.Epsilon)
+        {
+            _rb.AddForce(horizontal * _horizontalSpeed * Time.deltaTime * Vector2.right);
+        }
+    }
+}
